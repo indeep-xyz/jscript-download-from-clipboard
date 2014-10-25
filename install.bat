76501035925f9d5b2148bf8459886ed1de62904b
indeep-xyz <indeep.xyz@gmail.com>
2014-10-25 19:39:26 +0900

    fix install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 
-set key_root=HKEY_CLASSES_ROOT\Directory\Background\shell\download-from-clipboard2
+set key_root=HKEY_CLASSES_ROOT\Directory\Background\shell\download-from-clipboard
 set bin_main=%cd%\bin\main.wsf
 
 reg add "%key_root%" /v "MUIVerb" /t "REG_SZ" /d "download from clipboard"
