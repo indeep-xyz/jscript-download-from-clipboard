78414f57c103fe5af368f65c04f620c2ffa972db
indeep-xyz <indeep.xyz@gmail.com>
2014-10-25 19:37:53 +0900

    add install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+cd /d %~dp0
+
+set key_root=HKEY_CLASSES_ROOT\Directory\Background\shell\download-from-clipboard2
+set bin_main=%cd%\bin\main.wsf
+
+reg add "%key_root%" /v "MUIVerb" /t "REG_SZ" /d "download from clipboard"
+reg add "%key_root%\Command" /ve /t "REG_SZ" /d "wscript ""%bin_main%"""
