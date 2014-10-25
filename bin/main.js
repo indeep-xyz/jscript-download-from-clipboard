2e6350071041ea02c2aa2584ac32df836a1d786e
indeep-xyz <indeep.xyz@gmail.com>
2014-10-26 00:36:10 +0900

    update for the validation in main.js (URL and outputPath)

--- bin/main.js
+++ bin/main.js
@@ -5,6 +5,6 @@ var outputPath = createOutputPath(url, tempSuffix);
 // WScript.echo(url);
 // WScript.echo(outputPath);
 
-if (url.length + outputPath.length > 0){
+if (url.length > 0 && outputPath.length > 0) {
   downloadFile(url, outputPath, tempSuffix);
 }
