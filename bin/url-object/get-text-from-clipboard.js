2fe3e4a13d2d0813f702cabf5e4d4c2ebe9560b6
indeep-xyz@gmail.com <indeep.xyz@gmail.com>
2016-10-15 22:17:28 +0900

    Remove a file bin/url-object/get-text-from-clipboard

--- bin/url-object/get-text-from-clipboard.js
+++
@@ -1,9 +0,0 @@
-/**
- * Get text from clipboard.
- *
- * @return [string] Text in clipboard
- */
-function getTextFromClipboard() {
-  var html = new ActiveXObject('htmlfile');
-  return html.parentWindow.clipboardData.getData('text');
-}
