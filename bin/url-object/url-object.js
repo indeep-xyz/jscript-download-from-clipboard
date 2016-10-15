6c12fa003bb5d43e9d318270c15ea522409735aa
indeep-xyz@gmail.com <indeep.xyz@gmail.com>
2016-10-15 22:17:28 +0900

    Refactor the class UrlObject

--- bin/url-object/url-object.js
+++ bin/url-object/url-object.js
@@ -7,35 +7,30 @@
 function UrlObject(origin) {
 
   // - - - - - - - - - - - - - - - - - - - - - -
-  // private functions - for URL - in UrlObject
+  // private functions - in UrlObject
 
+  /**
+   * Check whether the argument is an URL or not.
+   *
+   * @private
+   * @method
+   * @param  {string}  str - String which is expected for an URL
+   * @return {boolean} Return true if the argument is an URL
+   */
   function checkUrl(str) {
     var re = new RegExp('/');
 
     return re.test(str);
   }
 
-  function extractProtocol(str) {
-    var re = new RegExp('^([^:]+?)://');
-    var ary = re.exec(str);
-
-    return (ary === null) ? 'http' : ary[1];
-  }
-
-  function extractHostname(str) {
-    var re = new RegExp('^([^:]+?://)?([^:/]+?)[:/]');
-    var ary = re.exec(str);
-
-    return (ary === null) ? '' : ary[2];
-  }
-
-  function extractPort(str) {
-    var re = new RegExp('^([^:]+?://)?[^:]+:(\\d+)/?');
-    var ary = re.exec(str);
-
-    return (ary === null) ? 80 : parseInt(ary[2], 10);
-  }
-
+  /**
+   * Extract string as a hash from an URL.
+   *
+   * @private
+   * @method
+   * @param  {string} str - An URL
+   * @return {string} Return a string as a hash
+   */
   function extractHash(str) {
     var re = new RegExp('#(.+)$');
     var ary = re.exec(str);
@@ -43,22 +38,49 @@ function UrlObject(origin) {
     return (ary === null) ? '' : ary[1];
   }
 
-  // - - - - - - - - - - - - - - - - - - - - - -
-  // private functions - others - in UrlObject
-
   /**
-   * Raise an error.
+   * Extract string as a hostname from an URL.
    *
    * @private
    * @method
-   * @param  {string} str - String for an URL
+   * @param  {string} str - An URL
+   * @return {string} Return a string as a hostname
    */
-  function failParsingUrl(str){
-    WScript.Echo(
-        "An error occurred while parsing by UrlObject.\n\n" +
-        "str:\n" + str
-        );
-    WScript.Quit(1);
+  function extractHostname(str) {
+    var re = new RegExp('^([^:]+?://)?([^:/]+?)[:/]');
+    var ary = re.exec(str);
+
+    return (ary === null) ? '' : ary[2];
+  }
+
+  /**
+   * Extract string as a port from an URL.
+   *
+   * @private
+   * @method
+   * @param  {string} str - An URL
+   * @return {string} Return a string as a port
+   */
+  function extractPort(str) {
+    var re = new RegExp('^([^:]+?://)?[^:]+:(\\d+)/?');
+    var ary = re.exec(str);
+
+    return (ary === null) ? 80 : parseInt(ary[2], 10);
+  }
+
+  /**
+   * Extract string as a protocol from an URL.
+   *
+   * @private
+   * @method
+   * @param  {string} str - An URL
+   * @return {string} Return a string as a protocol
+   */
+  function extractProtocol(str) {
+    var re = new RegExp('^([^:]+?)://');
+    var ary = re.exec(str);
+
+    return (ary === null) ? 'http' : ary[1];
   }
 
   // - - - - - - - - - - - - - - - - - - - - - -
