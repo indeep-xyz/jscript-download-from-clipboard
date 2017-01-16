10c88bee6506f241e409a5edae53beb496ddc385
indeep-xyz <indeep.xyz@gmail.com>
2017-01-17 00:05:54 +0900

    Add the comments into SoundIndicator

--- bin/sound-indicator/sound-indicator.js
+++ bin/sound-indicator/sound-indicator.js
@@ -1,22 +1,52 @@
+/**
+ * This class supports to indicate with sound.
+ *
+ * @class
+ * @constructor
+ * @param  {object} options                  - Options for indication with sound.
+ * @param  {string} options.soundAtCompleted - A path of a sound file to indicate.
+ */
 function SoundIndicator(options) {
   this.soundFilePath = options.soundAtCompleted;
 }
 
-SoundIndicator.prototype.soundFilePath = '';
-
 /**
  * @public
  * @static
+ * @type {string} The defalut path of a sound file.
+ */
+SoundIndicator.prototype.soundFilePath = '';
+
+/**
+ * Play sound file.
+ *
+ * @public
+ * @static
+ * @method
  */
 SoundIndicator.prototype.playSound = function() {
   // - - - - - - - - - - - - - - - - - - - - - -
   // private functions - in SoundIndicator.prototype.playSound
 
+  /**
+   * Check whether a file is at the argument path.
+   *
+   * @private
+   * @method
+   * @param  {string} path - A path expected as a sound file.
+   */
   function doesExist(path) {
     var fs = new ActiveXObject('Scripting.FileSystemObject');
     return fs.FileExists(path);
   }
 
+  /**
+   * Play a sound file at the argument path.
+   *
+   * @private
+   * @method
+   * @param  {string} path - A path expected as a sound file.
+   */
   function play(path) {
     var player = WScript.CreateObject("MediaPlayer.MediaPlayer");
     player.FileName = path;
